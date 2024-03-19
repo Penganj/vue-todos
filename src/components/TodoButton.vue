92252b38d3267c527b6717b7e70d7754b2d72ae5
ismailkurt <ismail.kurt7749@gmail.com>
2024-03-19 14:54:08 +0300

    todo Slots

---
+++ src/components/TodoButton.vue
@@ -0,0 +1,14 @@
+<script setup></script>
+
+<template>
+    <button>
+        <slot>Create</slot>
+    </button>
+</template>
+
+<style scoped>
+    button {
+        padding: 8px 16px;
+        border: none;
+    }
+</style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,24 @@
+<script setup>
+const props = defineProps({
+    todo: {
+        type: Object,
+        required: true,
+    }
+})
+</script>
+
+<template>
+    <li>
+        <input type="checkbox" :checked="todo.isCompleted" />
+        <div class="todo">
+            <input type="text" :value="todo.todo" />
+            <span>
+                {{ todo.todo }}
+            </span>
+        </div>
+    </li>
+</template>
+
+<style scoped>
+
+</style>

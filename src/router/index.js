942cd1a605f56271c02a325be07a6d697bd69d75
ismailkurt <ismail.kurt7749@gmail.com>
2024-03-21 13:11:04 +0300

    Vue todo end

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import {createRouter, createWebHistory} from "vue-router"
 import TodosView from "@/views/TodosView.vue"
 import AboutView from "@/views/AboutView.vue"
+import TestingView from "@/views/TestingView.vue"
 
 const router = createRouter ({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter ({
             path: "/about",
             name: 'about',
             component: AboutView
+        },
+        {
+            path: "/testing",
+            name: "testing",
+            component: TestingView
         }
     ]
 })
